62d49daf7ba654a8f9cd1360843d422dfeeb506a
otavioabreu27 <otavio.abreu96@gmail.com>
2023-06-14 10:48:50 -0300

    remoção de clientes

--- back/src/modules/cliente/cliente.controller.ts
+++ back/src/modules/cliente/cliente.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { Cliente } from './cliente.entity';
 
@@ -27,6 +27,20 @@ export class ClienteController {
     try {
       const resp = this.clienteService.criaCliente(cliente);
       return resp;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  @Delete(':id')
+  async deletaCliente(@Param('id') id: string): Promise<string> {
+    try {
+      const resp = await this.clienteService.deletaCliente(id);
+      if (resp.affected != 0) {
+        return 'Deletado';
+      } else {
+        return 'Nenhuma linha afetada';
+      }
     } catch (e) {
       return e;
     }
--- back/src/modules/cliente/cliente.service.ts
+++ back/src/modules/cliente/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cliente } from './cliente.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class ClienteService {
@@ -24,5 +24,9 @@ export class ClienteService {
 
   async criaCliente(cliente: Cliente): Promise<Cliente> {
     return this.Cliente.save(cliente);
+  }
+
+  async deletaCliente(id: string): Promise<DeleteResult> {
+    return this.Cliente.delete(parseInt(id));
   }
 }
